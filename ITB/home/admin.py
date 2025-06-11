36bac7aac6b86852da1d957798a01e39fd6e6e9a
SaadKiyani0011 <saadkiyanioffical@gmail.com>
2025-06-11 12:33:02 +0500

    Initial commit for Django project

--- ITB/home/admin.py
+++ ITB/home/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from home.modals import Course, MissionVision, TeamMember
 
-# Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'order', 'is_featured')
+    list_editable = ('order', 'is_featured')
+
+@admin.register(MissionVision)
+class MissionVisionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(TeamMember)
+class TeamMemberAdmin(admin.ModelAdmin):
+    list_display = ('name', 'position', 'is_ceo')
+    list_editable = ('is_ceo',)
--- ITB/home/views.py
+++ ITB/home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from home.modals import Course, MissionVision, TeamMember
 
-# Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    context = {
+        'courses': Course.objects.filter(is_featured=True),
+        'mission_vision': MissionVision.objects.first(),
+        'ceo': TeamMember.objects.filter(is_ceo=True).first()
+    }
+    return render(request, 'home.html', context)
